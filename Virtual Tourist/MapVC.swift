77c558f1065f804b5571d25ad523cc6bde8e0e67
Roman Sheydvasser <s.roman@gmail.com>
2017-04-10 01:39:57 -0700

    userdefaults save map location

--- Virtual Tourist/MapVC.swift
+++ Virtual Tourist/MapVC.swift
@@ -9,13 +9,31 @@
 import UIKit
 import MapKit
 
-class MapVC: UIViewController {
+class MapVC: UIViewController, MKMapViewDelegate {
     
     @IBOutlet weak var mapView: MKMapView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        mapView.delegate = self
+        loadLastMapView()
     }
-
+    
+    func loadLastMapView() {
+        if let _ = UserDefaults.standard.value(forKey: "mapLatitude") {
+            let span = MKCoordinateSpanMake(UserDefaults.standard.value(forKey: "mapLatitudeDelta") as! CLLocationDegrees, UserDefaults.standard.value(forKey: "mapLongitudeDelta") as! CLLocationDegrees)
+            let centerCoord = CLLocationCoordinate2DMake(UserDefaults.standard.value(forKey: "mapLatitude") as! CLLocationDegrees, UserDefaults.standard.value(forKey: "mapLongitude") as! CLLocationDegrees)
+            let region = MKCoordinateRegionMake(centerCoord, span)
+            mapView.setRegion(region, animated: true)
+        }
+    }
+    
+    func mapView(_ mapView: MKMapView, regionDidChangeAnimated animated: Bool) {
+        //save map view so same place will be loaded when app is reopened
+        UserDefaults.standard.set(mapView.region.center.latitude as Double, forKey: "mapLatitude")
+        UserDefaults.standard.set(mapView.region.center.longitude as Double, forKey: "mapLongitude")
+        UserDefaults.standard.set(mapView.region.span.latitudeDelta as Double, forKey: "mapLatitudeDelta")
+        UserDefaults.standard.set(mapView.region.span.longitudeDelta as Double, forKey: "mapLongitudeDelta")
+    }
 }
 
