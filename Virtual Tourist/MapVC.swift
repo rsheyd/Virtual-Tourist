c1fdbaeeeaeed5f0be360ba664f5bccc4cdf8029
Roman Sheydvasser <s.roman@gmail.com>
2017-04-09 22:16:06 -0700

    new VC and UI objects

---
+++ Virtual Tourist/MapVC.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Virtual Tourist
+//
+//  Created by Roman Sheydvasser on 4/9/17.
+//  Copyright © 2017 RLabs. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class MapVC: UIViewController {
+    
+    @IBOutlet weak var mapView: MKMapView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
+
---
+++ Virtual Tourist/PhotoAlbumVC.swift
@@ -0,0 +1,24 @@
+//
+//  PhotoAlbumVC.swift
+//  Virtual Tourist
+//
+//  Created by Roman Sheydvasser on 4/9/17.
+//  Copyright © 2017 RLabs. All rights reserved.
+//
+
+import UIKit
+
+class PhotoAlbumVC: UIViewController {
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    @IBOutlet weak var newCollectionButton: UIBarButtonItem!
+    
+    @IBAction func newCollectionPressed(_ sender: Any) {
+    
+    }
+    
+    
+    
+    
+}
